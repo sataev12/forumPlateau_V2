76ba463034e8340180069e234cde4a2ebdc80ecc
sataev12 <sataevmagomed.eu@gmail.com>
2024-04-22 16:57:35 +0200

    en cour de creation de l'inscription pour les utilisateurs

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -3,11 +3,25 @@ namespace Controller;
 
 use App\AbstractController;
 use App\ControllerInterface;
+use Model\Managers\UtilisateurManager;
 
 class SecurityController extends AbstractController{
     // contiendra les méthodes liées à l'authentification : register, login et logout
 
-    public function register () {}
+    public function register () {
+        if($_POST["submit"]) {
+
+            // Filtrer la saisie des champs du formulaire d'inscription
+            $pseudonyme = filter_input(INPUT_POST, "pseudonyme", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $motDePasse = filter_input(INPUT_POST, "motDePasse", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $motDePasse2 = filter_input(INPUT_POST, "motDePasse2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            if($pseudonyme && $email && $motDePasse && $motDePasse2) {
+                
+            }
+        }
+    }
     public function login () {}
     public function logout () {}
 }
